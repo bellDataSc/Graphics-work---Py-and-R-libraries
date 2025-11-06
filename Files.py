78a30a0b32326c56e316c2b1b35176d31243f9c8
Isabel Cruz <93488292+bellDataSc@users.noreply.github.com>
2025-11-06 18:18:00 -0300

    Add Files.py for analysis data and code references

---
+++ Files.py
@@ -0,0 +1,8 @@
+#Files I used for the analysis
+#Note: These do not contain the same names I used in the work.
+#cod: and cod with family groupings to which they belong
+#Data history: This refers to the historical data, which encompasses a national flow of information, including locations and prices, to be analyzed and used for statistical purposes.
+
+data_history_file = "/content/data history.xlsx"
+code_file = "/content/code.xlsx"
+
