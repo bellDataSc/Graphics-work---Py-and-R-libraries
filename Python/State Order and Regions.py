7b4dec082d3f74b362263a3014efba4d630aae44
Isabel Cruz <93488292+bellDataSc@users.noreply.github.com>
2025-11-06 18:19:56 -0300

    Add state order and regions data structure

---
+++ Python/State Order and Regions.py
@@ -0,0 +1,15 @@
+state_order = [
+    "AC","AM","AP","PA","RO","RR","TO",    # North
+    "AL","BA","CE","MA","PB","PE","PI","RN","SE",  # Northeast
+    "ES","MG","RJ","SP",                   # Southeast
+    "PR","RS","SC",                        # South
+    "DF","GO","MS","MT"                    # Center-West
+]
+
+regions = {
+    "North": ["AC","AM","AP","PA","RO","RR","TO"],
+    "Northeast": ["AL","BA","CE","MA","PB","PE","PI","RN","SE"],
+    "Southeast": ["ES","MG","RJ","SP"],
+    "South": ["PR","RS","SC"],
+    "Center-West": ["DF","GO","MS","MT"]
+}
