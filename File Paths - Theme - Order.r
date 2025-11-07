6af5b32ac21c3a03b6aa4f71ecd6e67c490c2c65
Isabel Cruz <93488292+bellDataSc@users.noreply.github.com>
2025-11-07 18:26:41 -0300

    Add initial data and state order definitions

---
+++ File Paths - Theme - Order.r
@@ -0,0 +1,21 @@
+data_history_file <- "/content/data history.xlsx"
+code_file <- "/content/code.xlsx"
+
+
+family_colors <- c("#4472C4", "#ED7D31", "#70AD47")
+
+state_order <- c(
+  "AC","AM","AP","PA","RO","RR","TO",       
+  "AL","BA","CE","MA","PB","PE","PI","RN","SE",  
+  "ES","MG","RJ","SP",                      
+  "PR","RS","SC",                          
+  "DF","GO","MS","MT"                         
+)
+
+regions <- list(
+  "North" = c("AC","AM","AP","PA","RO","RR","TO"),
+  "Northeast" = c("AL","BA","CE","MA","PB","PE","PI","RN","SE"),
+  "Southeast" = c("ES","MG","RJ","SP"),
+  "South" = c("PR","RS","SC"),
+  "Center-West" = c("DF","GO","MS","MT")
+)
